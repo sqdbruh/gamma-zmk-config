01b6e2a4e89b51f9a540a613c5dc2e7fa776706b
Grigory Pavlichenko <hardtohandle@yandex.ru>
2026-01-19 16:09:12 +0300

    marker fix

--- config/gamma_led_probe.c
+++ config/gamma_led_probe.c
@@ -10,11 +10,13 @@
 #define PROBE1_PIN NRF_GPIO_PIN_MAP(0, 7)
 #define LED_GATE_PIN NRF_GPIO_PIN_MAP(0, 6)
 
+__attribute__((used, section(".rodata.gamma_marker")))
 static const char gamma_probe_marker[] = GAMMA_PROBE_MARKER;
+__attribute__((used))
+static volatile const char *gamma_probe_marker_ptr = gamma_probe_marker;
 
 static int gamma_led_probe_init(void) {
-    volatile char marker = gamma_probe_marker[0];
-    (void)marker;
+    (void)gamma_probe_marker_ptr;
 
     nrf_gpio_cfg_output(LED_GATE_PIN);
     nrf_gpio_pin_set(LED_GATE_PIN);
